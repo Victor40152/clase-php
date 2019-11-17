c7b225af2b9f712111f10ecad9cc8ad636d520a6
victor manuel echavarria <vicme93@gmail.com>
2019-11-17 12:53:51 -0500

    agrega cambio hoja de estilo

---
+++ Biografia_DIM.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head> 
+     <title>Biografia del DIM </title>
+     <link rel="stylesheet" type="text/css" href="css/estilos.css">
+    </head>
+    <body>
+        <h1>Biografia del DIM</h1>
+        <h2>Historia del Decano</h2>
+        <p>El Deportivo Independiente Medellín, más conocido como Independiente Medellín, DIM, o simplemente Medellín, es un club de fútbol colombiano fundado bajo el nombre de «Medellín Football Club» el 14 de noviembre de 1913 por Alberto Uribe Piedrahíta. Perteneciente a la ciudad de Medellín, capital del departamento de Antioquia. Actualmente juega en la Categoría Primera A del Fútbol Profesional Colombiano. Disputa los partidos como local en el Estadio Atanasio Girardot.2​ Su propietario es el Equipo del Pueblo S. A. en cabeza de Raúl Giraldo como máximo accionista de la compañía.
+        Durante el amateurismo, el club fue ocho veces campeón nacional, torneo avalado por la confederación Sudamericana de fútbol. A su vez, logró ocho veces el campeonato de la Liga Antioqueña de Primera División, consolidándose de esta forma como el club más veces campeón de este ámbito.2​
+        Desde el inicio del profesionalismo en 1948, el Independiente Medellín ha obtenido seis veces el campeonato colombiano (1955, 1957, 2002-II, 2004-I, 2009-II, y 2016-I), y en dos ocasiones la Copa Colombia (1981 y 2019). Ha faltado en cuatro temporadas de la Categoría Primera A: En 1952, 1953, 1958, 1971.
+        Es uno de los equipos más representativos del país, con una imponente hinchada y por sus filas han pasado figuras de la talla de Orestes Omar Corbatta y José Manuel Moreno, más conocido como el ''Charro''.</p>
+
+     <br>
+     <img src="rojo.jpg">
+     <br>
+     <a href="index.php" id="imagen2">volver</a>
+
+</body>
+
+
+</html>
---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html> 
+<html>
+    <head>
+        <title>
+            Hola Mundo
+        </title>
+     <link rel="stylesheet" type="text/css" href="css/estilos.css">
+
+
+    </head>
+    <body>
+
+        <!-- Esto es un Comentario -->
+        <h1>Hola Mundo, Me llamo Victor! </h1>
+        <h2>Subtitulo</h2>
+        <h3>Contenido</h3>
+        <h4>texto pqño</h4>
+        <h5>texto mas pequeño
+        </h5>
+        <h6>Muy pequeño</h6>
+        <a href="http://www.google.com" target="_blank">click aqui para ir a google</a>
+        <br>
+         Hola Mundo 2.
+         <br>
+         Este es otro Texto.
+         <br>
+
+        <a href="Biografia_DIM.php"> <img id="imagen1" src="DIm-106-años-600x600.jpg"></a>
+    </body>
+</html>
